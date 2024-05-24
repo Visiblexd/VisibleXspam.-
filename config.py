cf24b88282c52571c578d8996c1ff443b3257eda
Visiblexd <bhushanshashank144@gmail.com>
2024-05-24 08:54:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,11 +10,11 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 
 # VALUES REQUIRED FOR XBOTS
-API_ID = 18136872
-API_HASH = "312d861b78efcd1b02183b2ab52a83a4"
+API_ID = 12160204
+API_HASH = "944053be988f83c2554a2b40429e90d7"
 CMD_HNDLR = getenv("CMD_HNDLR", default=".")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
-HEROKU_API_KEY = getenv("HEROKU_API_KEY", "cb2147ff-d743-49fc-a18e-6a40aec75e77")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY", "9d358c5e-3baf-465d-b00a-663e8bbf0db4")
 
 BOT_TOKEN = getenv("BOT_TOKEN", default=None)
 BOT_TOKEN2 = getenv("BOT_TOKEN2", default=None)
@@ -27,10 +27,10 @@ BOT_TOKEN8 = getenv("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = getenv("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = getenv("BOT_TOKEN10", default=None)
 
-SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="7043791176").split()))
+SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="7048922897").split()))
 for x in ALTRON:
     SUDO_USERS.append(x)
-OWNER_ID = int(getenv("OWNER_ID", default="6348268237"))
+OWNER_ID = int(getenv("OWNER_ID", default="7048922897"))
 SUDO_USERS.append(OWNER_ID)
 
 
